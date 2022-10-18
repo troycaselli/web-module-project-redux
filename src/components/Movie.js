3f16e007156697b56dcf93e7c6e1990954f13a4c
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-10-18 09:31:58 -0700

    hooked up deleteMovie action creator to movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import {connect} from 'react-redux';
 
+import {deleteMovie} from '../actions/movieActions';
+
 const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
 
     const movie = props.movies.find(movie=>movie.id===Number(id));
     
+    const deleteMovieHandler = () => {
+        props.deleteMovie(parseInt(id));
+        push('/movies/');
+    }
+
     return(<div className="modal-page col">
         <div className="modal-dialog">
             <div className="modal-content">
@@ -38,7 +45,12 @@ const Movie = (props) => {
                         
                         <section>
                             <span className="m-2 btn btn-dark">Favorite</span>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                            <span className="delete"><input 
+                                    type="button" 
+                                    className="m-2 btn btn-danger" 
+                                    value="Delete" 
+                                    onClick={deleteMovieHandler}
+                                /></span>
                         </section>
                     </div>
                 </div>
@@ -53,4 +65,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, {})(Movie);
+export default connect(mapStateToProps, {deleteMovie})(Movie);
